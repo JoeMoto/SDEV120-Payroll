8bffc73f197e310bc1220b67b08bf2f2c0b2c305
JoeMoto <jmoto@ivytech.edu>
2026-07-05 21:01:44 -0400

    Created Main file with modules

---
+++ # main.py
@@ -0,0 +1,24 @@
+# main.py
+# Payroll Program
+
+def main():
+    print("   Employee Payroll System\n")
+    
+def hourlyRate():
+
+    
+def hourlyRate():
+    
+
+def grossPay():
+    
+    
+def taxes():
+    
+    
+def netPay():
+    
+    
+def employeeOutput():
+    
+    
